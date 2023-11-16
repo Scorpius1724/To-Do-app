799b71d1ae818ef52e0889a5211335b402f4ecf5
Swapnil <swapnilacharjee04@gmail.com>
2023-11-17 01:23:08 +0530

    Implement completing tasks feature - Day 19

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ import functions
 todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state['new_todo'] + '\ns'
+    todo = st.session_state['new_todo'] + '\n'
     todos.append(todo)
     functions.write_todos(todos)
 
@@ -12,8 +12,13 @@ st.title("To-Do App")
 st.subheader("This is my To-Do App")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 
 st.text_input(label="", placeholder="Add new to-do...", on_change=add_todo, key='new_todo')
